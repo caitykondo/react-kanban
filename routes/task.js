ca6a0f10c3552bbd76c32705c335a7f2220617d2
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-03-21 18:44:26 -1000

    post to / to create new card

--- routes/task.js
+++ routes/task.js
@@ -3,9 +3,29 @@ let router = express.Router();
 const db = require('./../models');
 const { Card } = db;
 
+// / GET ALL CARDS
+// / POST NEW CARD
+
+// /{id} GET CARD BY ID
+// /{id} DELETE CARD
+// /{id} PUT EDIT CARD
+
 router.route('/')
   .get((req, res) => {
-    res.send('hello world');
-  });
+    Card.findAll()
+    .then((cards) => {
+      res.send(cards);
+    })
+  })
+  .post((req, res) => {
+    // add check for all required fields
+    Card.create({
+      task : req.body.task,
+      priority : req.body.priority,
+      status : req.body.status,
+      createdBy : req.body.createdBy,
+      assignedTo : req.body.assignedTo
+    })
+  })
 
 module.exports = router;
