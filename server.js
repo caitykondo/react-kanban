bd2730d842ac190b1d2d5d91ff564cedbcfd82d1
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-03-25 15:03:23 -1000

    fixed issue with AccessControl Headers

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const task = require('./routes/task');
 
 app.use(bodyParser.urlencoded({ extended : true }));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*")
+  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept")
+  next();
+});
+
 app.use('/task', task);
 
 
